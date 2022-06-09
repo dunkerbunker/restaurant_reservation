44bf4a3fe8b492e03d9fc158907faf18f1a0f076
Yoosuf Sayyid <cena26657@gmail.com>
2022-06-09 19:11:49 +0500

    init

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth', 'admin']) -> name['admin.']->prefix['admin']->group(fucntion() {
+
+});
+
+require __DIR__.'/auth.php';
